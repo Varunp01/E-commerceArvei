d7c94069b4eec45f51a6c21ea6ca2e843b7ee7fd
Varunp01 <152736714+Varunp01@users.noreply.github.com>
2024-08-16 14:50:05 +0530

    Update CategoryBox.jsx

--- src/categorycomponents/CategoryBox.jsx
+++ src/categorycomponents/CategoryBox.jsx
@@ -4,7 +4,7 @@ export default function CategoryBox({category,option}){
     return(
         <>
         <div className="cardoutline">
-          {option=="1"?<img className="cardimg" src="https://ibb.co/tbK6ZrR" alt="Card image"></img> : <img className="cardimg" src="src\trendingcomponents\foodCategory.PNG" alt="Card image"></img>}
+          {option=="1"?<img className="cardimg" src="https://ibb.co/tbK6ZrR" alt="Card image"></img> : <img className="cardimg" src="https://ibb.co/72PmtZv" alt="Card image"></img>}
           <br></br>
               <div className="cardbody">
                 <p className="cardtext">{category}</p>
